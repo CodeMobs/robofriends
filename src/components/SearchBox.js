3bc82c54b0933ca9102c7b1269d1e2cfba59db4b
Chris <info@chrisgkalfas.com>
2021-11-28 02:10:54 +0200

    Finalize the search function

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({ searchfield, searchChange }) => {
 
     return (
-        <div>
+        <div className='pa2'>
             <input 
                 className='pa3 ba b--green bg-lightest-blue'
                 type='search' 
                 placeholder='Search Robots'
+                onChange={searchChange}
             />
         </div>
         
-
     );
 }
 
