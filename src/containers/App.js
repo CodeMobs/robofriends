1cc9f6d51f77b06bf1fb183541029d5df431639b
Chris <info@chrisgkalfas.com>
2021-12-02 01:43:39 +0200

    Implementing the app using github API

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,53 +1,79 @@
-import React, { Component } from 'react';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll'
- import './App.css';
+import React, { useState, useEffect } from "react";
+import Card from "../components/Card";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import "./App.css";
+import axios from "axios";
 
- class App extends Component {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [finalUsers, setFinalUsers] = useState([]);
+  const finalUserdata = [];
 
+  // Here we re fetching the first 10 initial users from the first endpoint
+  const fetchIntialUsers = async () => {
+    await axios
+      .get(
+        "https://api.github.com/search/users?q=language:javascript+type:user&sort=followers&order=desc&page=1&per_page=10"
+      )
+      .then((resp) => {
+        setUsers(resp.data.items);
+      });
+  };
 
-    constructor(){
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
+  // this is the function for fetching data for a specific userbased on a url from the second endpoint
+  const fetchDataPerUser = async (url) => {
+    // i construct an array  with all the data i get for every user
+    await axios.get(url).then((resp) => finalUserdata.push(resp.data));
+  };
 
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => this.setState({ robots: users }));
-    }
+  useEffect(() => {
+    fetchIntialUsers();
+    users.forEach((user) => fetchDataPerUser(user.url));
+    // this is a small hack to  make sure that react will update the state before rendering the data
+    setFinalUsers(finalUserdata);
+  }, []);
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value} );
-    }
-
-    render (){
-    const { robots, searchfield } = this.state; 
-    const filteredRobots = this.state.robots.filter(robot =>{
-        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    })
-
-    return !robots.length ? <h1>Loading ...</h1> :
-        (
-        <div className='tc'>
-            <h1> RoboFriends </h1>
-            <div className='search'>
-                <SearchBox searchChange = {this.onSearchChange} />
-            </div>
-            <div>
-                <Scroll>
-                <CardList robots = { filteredRobots } />
-                </Scroll>
-            </div>
-
-        </div>
-        
-        );
-    }
-  }
-
+  return (
+    <div>
+      <CardList robots={finalUsers} />
+    </div>
+  );
+};
 export default App;
+
+// componentDidMount(){
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(response => response.json())
+//     .then(users => this.setState({ robots: users }));
+// }
+
+// onSearchChange = (event) => {
+//     this.setState({ searchfield: event.target.value} );
+// }
+
+// render (){
+// const { robots, searchfield } = this.state;
+// const filteredRobots = this.state.robots.filter(robot =>{
+//     return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+// })
+
+// return !robots.length ? <h1>Loading ...</h1> :
+//     (
+//     <div className='tc'>
+//         <h1> RoboFriends </h1>
+//         <div className='search'>
+//             <SearchBox searchChange = {this.onSearchChange} />
+//         </div>
+//         <div>
+//             <Scroll>
+//             <CardList robots = { filteredRobots } />
+//             </Scroll>
+//         </div>
+
+//     </div>
+
+//     );
+//     }
+//   }
